1f71427b09e3cd620e1a4f3946cb7a0afd91f242
RustamU <70314107+RustamU@users.noreply.github.com>
2020-10-25 18:39:39 +0300

    Add files via upload

---
+++ Week 1/mygraph.py
@@ -0,0 +1,76 @@
+class Node():
+    def __init__(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge():
+    """ src and dst are Node type"""
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+    def getSource(self):
+        return self.src
+    def getDestination(self):
+        return self.dest
+    def __str__(self):
+        return self.src.getName() + '->' + self.dest.getName()
+
+class Digraph():
+    def __init__(self):
+        self.edges = {}
+    def addNode(self, node):
+        if node in self.edges:
+            raise ValueError ('node already in digraph')
+        else:
+            self.edges[node] = []
+
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not (src in self.edges and dest in self.edges):
+            raise ValueError ('node not in graph')
+        self.edges[src].append(dest)
+
+    def childrenOf(self, node):
+        return self.edges[node]
+    def hasNode(self, node):
+        return node in self.edges
+    def getNode(self, name):
+        for n in self.edges:
+            if n.getName() == name:
+                return n
+        raise NameError(name)
+    def __str__(self):
+        result = ''
+        for src in self.edges:
+            for dest in self.edges[src]:
+                result = result + src.getName() + '->' + dest.getName() + '\n'
+        return result
+
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+
+def buildCityGraph(graphType):
+    g = graphType()
+    cityList = ['Питер', 'Providence', 'New York', 'Chicago', 'Denver', 'Phoenix', 'Los Angeles']
+    for city in cityList:
+        g.addNode(Node(city))
+    g.addEdge(Edge(g.getNode('Питер'), g.getNode('Providence')))
+    g.addEdge(Edge(g.getNode('Питер'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Питер')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Питер')))
+    return g
+
+print(buildCityGraph(Digraph))
